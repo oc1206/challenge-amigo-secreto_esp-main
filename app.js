5bf3e5344abfddc481443ded253e0b0506e9cfb9
chris <chbnmultinegocios@gmail.com>
2025-08-17 17:48:54 -0500

    Agregamos la función agregarAmigo() y agregamos un id en el boton Agregar

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 let amigos = [];
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById(addfriend).value;
+
+    if (nombreAmigo === null) {
+        alert("Por favor, inserta un nombre de amigo.");
+    } else if (nombreAmigo === "") {
+        alert("Por favor, inserta un nombre de amigo.");
+    } else {
+        amigos.push(nombreAmigo);
+        document.getElementById(addfriend).value = "";
+    }
+    return amigos;
+}
